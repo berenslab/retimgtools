b4f8b8c24748478ccc7a7e5892c31f73537be6c1
Ziwei Huang <huang-ziwei@outlook.com>
2023-07-06 14:00:30 +0200

    change: radio buttons arrangement in `evaluate`

--- populate-evaluate-bagnet-task1.py
+++ populate-evaluate-bagnet-task1.py
@@ -40,14 +40,14 @@ task = Task(
 task.save()
 
 all_images = sorted(glob.glob("media/evaluate/BagNet/Task1/*.png"))
-# num_img_set = len(set([img[:-4] for img in all_images]))
 num_images = len(all_images)
 for i, img in enumerate(all_images):
+    j = i + 1
     q1 = task.question_set.create(
-        description=f"Q{i}/{num_images} [1/2]: Diabetic Retinopathy referral",
+        description=f"Q{j}/{num_images} [1/2]: Diabetic Retinopathy referral",
         image1=f"{img[6:]}",
         created_at=timezone.now(),
-        slug=f"{alias}-q{i}p1",
+        slug=f"{alias}-q{j}p1",
     )
 
     for choice in ["No DR", "Referable DR"]:
@@ -57,10 +57,10 @@ for i, img in enumerate(all_images):
         )
 
     q2 = task.question_set.create(
-        description=f"Q{i}/{num_images} [2/2]: How confident are you of the assigned grade?",
+        description=f"Q{j}/{num_images} [2/2]: How confident are you of the assigned grade?",
         created_at=timezone.now(),
         image1=f"{img[6:]}",
-        slug=f"{alias}-q{i}p2",
+        slug=f"{alias}-q{j}p2",
     )
 
     for choice in range(1, 11):
